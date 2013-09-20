30d0ab768bef1b1176edde741d23513a49690e53
root <root@brainpi.(none)>
2013-09-20 16:22:08 +0200

    binario para leer datos de sensor y guardar en rrd

--- rx-example.cpp
+++ rx-example.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 #include "RF24.h"
 #include "RF24Network.h"
-
+#include <ctime>
+#include <stdio.h>
+#include <rrd.h>
+/**
+ * g++ -L/usr/lib main.cc -I/usr/include -o main -lrrd
+ **/
 using namespace std;
 
 // CE Pin, CSN Pin, SPI Speed
@@ -26,20 +31,29 @@ const uint16_t other_node = 1;
 // Structure of our payload
 struct payload_t
 {
-  unsigned long ms;
-  unsigned long counter;
+  unsigned long  nodeId;
+  float temperature;
+  float humidity;
 };
 
 int main(int argc, char** argv) 
 {
-
 	// Refer to RF24.h or nRF24L01 DS for settings
+	char    *rrd_argv[2];
+    	int       rrd_argc;
+    	char filename[] = "/root/weather/weather_sensor.rrd";
+    	char command[] = "update";
+	char values[100];
+
 	radio.begin();
-	delay(1);
-	network.begin(/*channel*/ 90, /*node address*/ this_node);
-	
+	radio.setDataRate(RF24_250KBPS);
+	radio.setRetries(7,7);
+	delay(5);
+	network.begin(/*channel*/ 50, /*node address*/ this_node);
 	while(1)
 	{
+		  //FILE * pFile;
+	          //pFile = fopen ("/root/temp-exterior.txt","a");
 		  network.update();
 		  while ( network.available() )
 		  {
@@ -47,17 +61,27 @@ int main(int argc, char** argv)
 		    RF24NetworkHeader header;
 		    payload_t payload;
 		    network.read(header,&payload,sizeof(payload));
-		    printf("Received packet #");
-		    printf("%lu", payload.counter);
-		    printf(" at ");
-		    printf("%lu\n", payload.ms);
-		    
+
+		    //time_t timer;
+    		    //char timeBuffer[25];
+    		    //struct tm* tm_info;
+    		    //time(&timer);
+    		    //tm_info = localtime(&timer);
+		    //strftime(timeBuffer, 25, "%Y/%m/%d %H:%M:%S", tm_info);
+		    //fprintf(pFile, "%s;%lu;%.2f;%.2f\n", timeBuffer,payload.nodeId, payload.temperature, payload.humidity);    
+		    rrd_argv[0]= command; 
+		    rrd_argv[1]= filename; 
+		    rrd_argv[3]= NULL;
+		    rrd_argc=3;
+		    printf("N:%.2f:%.2f\n", payload.temperature, payload.humidity);    
+		    sprintf(values, "N:%.2f:%.2f", payload.temperature, payload.humidity);    
+		    rrd_argv[2]= values;
+		    rrd_update(rrd_argc, rrd_argv);
 		  }
-		 //printf("out\n");
 		 delay(2000);
+		 //fclose(pFile);
 	}
 
-	
 	return 0;
 }
 
